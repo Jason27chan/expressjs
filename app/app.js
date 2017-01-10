ff30015881e58cc292d1ae5bd66e063a0794d848
Jason Chan <jason27chan@gmail.com>
2017-01-10 00:16:12 -0500

    fixed formatting

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 
- +var http = require('http');
- +
- +var myServer = http.createServer(function(request, response) {
- +    response.writeHead(200,{"Content-Type" : "text/plain"})
- +    response.write('Roux Meetups');
- +    response.end();
- +});
- +
- +myServer.listen(3000);
+var http = require('http');
+
+var myServer = http.createServer(function(request, response) {
+    response.writeHead(200,{"Content-Type" : "text/plain"})
+    response.write('Roux Meetups');
+    response.end();
+});
+
+myServer.listen(3000);
